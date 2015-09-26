d1e9efe30aa3c1b528deabc8b3fda7086d912964
Leo Dion <leogdion@brightdigit.com>
2015-09-25 23:22:27 -0400

    updated with GZIP

--- Marseft/Marseft.h
+++ Marseft/Marseft.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char MarseftVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Marseft/PublicHeader.h>
 
-#import "GZIP.h"
+#import "NSData+GZIP.h"
